067317613192a7a4f1b3455e5b69438ee504c013
Tommaso Laurenzi <tommaso.laurenzi@gmail.com>
2022-05-19 13:31:32 +0200

    fix(TSVariable): link TSVariable to Normal instead of clearing

--- lua/kanagawa/hlgroups.lua
+++ lua/kanagawa/hlgroups.lua
@@ -218,7 +218,7 @@ function M.setup(colors, config)
         -- TSSymbol                       = {},
         -- TSType                         = {},
         -- TSTypeBuiltin                  = {},
-        TSVariable                        = { fg = "NONE" },
+        TSVariable                        = { link = 'Normal' },
         TSVariableBuiltin                 = { fg = colors.sp2, style = config.variablebuiltinStyle },
 
         -- TSTag                          = {},
