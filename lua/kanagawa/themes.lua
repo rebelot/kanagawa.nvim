0743ed53a4c5de53e875421fe4db15136293ccf8
Tommaso Laurenzi <tommaso.laurenzi@gmail.com>
2022-08-16 17:07:58 +0200

    chore(annotations): add EmmyLua-style annotations to theme

--- lua/kanagawa/themes.lua
+++ lua/kanagawa/themes.lua
@@ -1,5 +1,48 @@
+--TODO: standardize theme color names
+
+---@class ThemeColors
+---@field bg KanagawaColors
+---@field bg_dim KanagawaColors
+---@field bg_dark KanagawaColors
+---@field bg_light0 KanagawaColors
+---@field bg_light1 KanagawaColors
+---@field bg_light2 KanagawaColors
+---@field bg_light3 KanagawaColors
+---@field bg_menu KanagawaColors
+---@field bg_menu_sel KanagawaColors
+---@field bg_status KanagawaColors
+---@field bg_visual KanagawaColors
+---@field bg_search KanagawaColors
+---@field fg_border KanagawaColors
+---@field fg_dark KanagawaColors
+---@field fg_reverse KanagawaColors
+---@field fg_comment KanagawaColors
+---@field fg KanagawaColors
+---@field fg_menu KanagawaColors
+---@field co KanagawaColors
+---@field st KanagawaColors
+---@field nu KanagawaColors
+---@field id KanagawaColors
+---@field fn KanagawaColors
+---@field sm KanagawaColors
+---@field kw KanagawaColors
+---@field op KanagawaColors
+---@field pp KanagawaColors
+---@field ty KanagawaColors
+---@field sp KanagawaColors
+---@field sp2 KanagawaColors
+---@field sp3 KanagawaColors
+---@field br KanagawaColors
+---@field re KanagawaColors
+---@field dep KanagawaColors
+---@field diag { error: KanagawaColors, warning: KanagawaColors, info: KanagawaColors, hint: KanagawaColors }
+---@field diff { add: KanagawaColors, delete: KanagawaColors, change: KanagawaColors, text: KanagawaColors }
+---@field git { added: KanagawaColors, removed: KanagawaColors, changed: KanagawaColors }
+
 return {
+    ---@param colors KanagawaColors
     default = function(colors)
+        ---@type ThemeColors
         return {
             bg = colors.sumiInk1,
             bg_dim = colors.sumiInk1b,
@@ -59,7 +102,7 @@ return {
                 added = colors.autumnGreen,
                 removed = colors.autumnRed,
                 changed = colors.autumnYellow,
-            }
+            },
         }
-    end
+    end,
 }
