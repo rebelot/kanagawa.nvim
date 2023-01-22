bd157a17172b21f4292405e33583b32569962584
Tommaso Laurenzi <tommaso.laurenzi@gmail.com>
2023-01-22 13:12:25 +0100

    fix(compile): compile to own directory

--- lua/kanagawa/utils.lua
+++ lua/kanagawa/utils.lua
@@ -1,17 +1,22 @@
 local M = {}
-local KANAGAWA_COMPILED_PATH =
-string.format("%s%s", vim.fn.stdpath("state"), vim.loop.os_uname().sysname:match("Windows") and "\\" or "/")
+local PATH_SEP = vim.loop.os_uname().sysname:match("Windows") and "\\" or "/"
+local KANAGAWA_COMPILED_PATH = string.format("%s%skanagawa%s", vim.fn.stdpath("state"), PATH_SEP, PATH_SEP)
 
 local KANAGAWA_COMPILED = function(theme)
-    return string.format("%skanagawa_%s_compiled.lua", KANAGAWA_COMPILED_PATH, theme)
+    return string.format("%s%s_compiled.lua", KANAGAWA_COMPILED_PATH, theme)
 end
 
+---@return string theme
 function M.get_theme_from_bg_opt()
     local config = require("kanagawa").config
     return config.theme[vim.o.background] or config.theme.default
 end
 
+---@param theme string
+---@param highlights table
 function M.compile(theme, highlights)
+    vim.loop.fs_mkdir(KANAGAWA_COMPILED_PATH, 448)
+
     local fname = KANAGAWA_COMPILED(theme)
     local file, err = io.open(fname, "wb")
     if not file or err then
@@ -23,9 +28,10 @@ function M.compile(theme, highlights)
         "require'kanagawa'.compiled = string.dump(function()",
         "local nvim_set_hl = vim.api.nvim_set_hl",
     }
+    local inspect = vim.inspect
     for hl, spec in pairs(highlights) do
         if next(spec) then
-            table.insert(lines, ('nvim_set_hl(0, "%s", %s)'):format(hl, vim.inspect(spec):gsub("%s", "")))
+            table.insert(lines, ('nvim_set_hl(0, "%s", %s)'):format(hl, inspect(spec):gsub("%s", "")))
         end
     end
     table.insert(lines, "end)")
@@ -36,6 +42,8 @@ function M.compile(theme, highlights)
     file:close()
 end
 
+---@param theme string
+---@return boolean status
 function M.load_compiled(theme)
     local f = loadfile(KANAGAWA_COMPILED(theme))
     if f then
