0d7e1178404e761fe4655dcaf0473ff1174b8b9c
Tommaso Laurenzi <tommaso.laurenzi@gmail.com>
2023-01-23 04:31:19 +0100

    raise error if colors.setup is called w/o theme and colorscheme is not set

--- lua/kanagawa/colors.lua
+++ lua/kanagawa/colors.lua
@@ -72,7 +72,11 @@ function M.setup(opts)
     opts = opts or {}
     local tbl_extend = vim.tbl_extend
     local override_colors = opts.colors or require("kanagawa").config.colors
-    local theme = opts.theme or require("kanagawa")._CURRENT_THEME
+    local theme = opts.theme or require("kanagawa")._CURRENT_THEME -- WARN: this fails if called before kanagawa.load()
+
+    if not theme then
+        error("kanagawa.colors.setup(): No theme specified. Either specify a theme or call this funciton after ':colorscheme kanagawa'")
+    end
 
     -- Add to and/or override palette_colors
     local updated_palette_colors = tbl_extend("force", palette_colors, override_colors.palette)
