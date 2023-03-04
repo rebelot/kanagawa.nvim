5173878eb6bea1bc50f01e143fc08db3c022cf6a
Tommaso Laurenzi <tommaso.laurenzi@gmail.com>
2023-03-04 22:58:57 +0100

    fix(lualine): fix lualine integration

--- lua/lualine/themes/kanagawa.lua
+++ lua/lualine/themes/kanagawa.lua
@@ -1,37 +1,37 @@
-local colors = require("kanagawa.colors").setup()
+local theme = require("kanagawa.colors").setup().theme
 
 local kanagawa = {}
 
 kanagawa.normal = {
-  a = { bg = colors.crystalBlue, fg = colors.bg_dark },
-  b = { bg = colors.winterBlue, fg = colors.crystalBlue },
-  c = { bg = colors.bg_light0, fg = colors.fg },
+  a = { bg = theme.crystalBlue, fg = theme.bg_dark },
+  b = { bg = theme.winterBlue, fg = theme.crystalBlue },
+  c = { bg = theme.bg_light0, fg = theme.fg },
 }
 
 kanagawa.insert = {
-  a = { bg = colors.autumnGreen, fg = colors.bg_dark },
-  b = { bg = colors.bg, fg = colors.autumnGreen },
+  a = { bg = theme.diag.ok, fg = theme.ui.bg },
+  b = { bg = theme.ui.bg, fg = theme.diag.ok },
 }
 
 kanagawa.command = {
-  a = { bg = colors.boatYellow2, fg = colors.bg_dark },
-  b = { bg = colors.bg, fg = colors.boatYellow2 },
+  a = { bg = theme.vcs.removed, fg = theme.ui.bg },
+  b = { bg = theme.ui.bg, fg = theme.vcs.removed },
 }
 
 kanagawa.visual = {
-  a = { bg = colors.oniViolet, fg = colors.bg_dark },
-  b = { bg = colors.bg, fg = colors.oniViolet },
+  a = { bg = theme.syn.fun, fg = theme.ui.bg },
+  b = { bg = theme.ui.bg, fg = theme.syn.fun },
 }
 
 kanagawa.replace = {
-  a = { bg = colors.autumnRed, fg = colors.bg_dark },
-  b = { bg = colors.bg, fg = colors.autumnRed },
+  a = { bg = theme.syn.constant, fg = theme.ui.bg },
+  b = { bg = theme.ui.bg, fg = theme.syn.constant },
 }
 
 kanagawa.inactive = {
-  a = { bg = colors.bg_status, fg = colors.crystalBlue },
-  b = { bg = colors.bg_status, fg = colors.fujiGray, gui = "bold" },
-  c = { bg = colors.bg_status, fg = colors.fujiGray },
+  a = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+  b = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim, gui = "bold" },
+  c = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
 }
 
 if vim.g.kanagawa_lualine_bold then
