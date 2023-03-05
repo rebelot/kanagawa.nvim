9446af54dded057ad1386bf6b662727f8606eb5d
Frank <fbearoff@users.noreply.github.com>
2023-03-04 19:34:17 -0500

    add navic hl groups

--- lua/kanagawa/highlights/plugins.lua
+++ lua/kanagawa/highlights/plugins.lua
@@ -190,6 +190,36 @@ function M.setup(colors, config)
 
         -- Lazy
         LazyProgressTodo = { fg = theme.ui.nontext },
+
+        -- Nvim-Navic
+        NavicIconsFile = { fg = theme.syn.punct },
+        NavicIconsModule = { fg = theme.syn.operator },
+        NavicIconsNamespace = { fg = theme.syn.punct },
+        NavicIconsPackage = { fg = theme.ui.special },
+        NavicIconsClass = { fg = theme.syn.constant },
+        NavicIconsMethod = { fg = theme.syn.fun },
+        NavicIconsProperty = { fg = theme.syn.type },
+        NavicIconsField = { fg = theme.diag.hint },
+        NavicIconsConstructor = { fg = theme.syn.constant },
+        NavicIconsEnum = { fg = theme.syn.regex },
+        NavicIconsInterface = { fg = theme.syn.identifier },
+        NavicIconsFunction = { fg = theme.syn.fun },
+        NavicIconsVariable = { fg = theme.syn.statement },
+        NavicIconsConstant = { fg = theme.syn.statement },
+        NavicIconsString = { fg = theme.syn.string },
+        NavicIconsNumber = { fg = theme.syn.number },
+        NavicIconsBoolean = { fg = theme.syn.constant },
+        NavicIconsArray = { fg = theme.syn.type },
+        NavicIconsObject = { fg = theme.syn.constant },
+        NavicIconsKey = { fg = theme.syn.statement },
+        NavicIconsNull = { fg = theme.syn.identifier },
+        NavicIconsEnumMember = { fg = theme.syn.identifier },
+        NavicIconsStruct = { fg = theme.syn.constant },
+        NavicIconsEvent = { fg = theme.syn.constant },
+        NavicIconsOperator = { fg = theme.syn.punct },
+        NavicIconsTypeParameter = { fg = theme.syn.special1 },
+        NavicText = { fg = theme.ui.fg },
+        NavicSeparator = { fg = theme.ui.fg },
     }
 end
 
