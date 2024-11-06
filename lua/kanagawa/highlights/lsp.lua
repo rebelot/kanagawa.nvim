bfba3e28faf3a9c1e9c887f191670d2cff321406
Tommaso Laurenzi <tommaso.laurenzi@gmail.com>
2024-11-06 11:03:13 +0100

    feat(lsp): add some missing types

--- lua/kanagawa/highlights/lsp.lua
+++ lua/kanagawa/highlights/lsp.lua
@@ -12,30 +12,32 @@ function M.setup(colors, config)
         -- ["@lsp.type.function"] = { link = "Function" },
         -- ["@lsp.type.interface"] = { link = "Structure" },
         ["@lsp.type.macro"] = { link = "Macro" },
-        ["@lsp.type.method"] = { link = "@function.method" },           -- Function
-        ["@lsp.type.namespace"] = { link = "@module" },                 -- Structure
-        ["@lsp.type.parameter"] = { link = "@variable.parameter" },     -- Identifier
+        ["@lsp.type.method"] = { link = "@function.method" },       -- Function
+        ["@lsp.type.namespace"] = { link = "@module" },             -- Structure
+        ["@lsp.type.parameter"] = { link = "@variable.parameter" }, -- Identifier
         -- ["@lsp.type.property"] = { link = "Identifier" },
         -- ["@lsp.type.struct"] = { link = "Structure" },
         -- ["@lsp.type.type"] = { link = "Type" },
         -- ["@lsp.type.typeParameter"] = { link = "TypeDef" },
-        ["@lsp.type.variable"] = { fg = 'none' },     -- Identifier
-        ["@lsp.type.comment"] = { fg = 'none' },      -- Comment
+        ["@lsp.type.variable"] = { fg = "none" }, -- Identifier
+        ["@lsp.type.comment"] = { fg = "none" },  -- Comment
 
+        ["@lsp.type.const"] = { link = "Constant" },
+        ["@lsp.type.comparison"] = { link = "Operator" },
+        ["@lsp.type.bitwise"] = { link = "Operator" },
+        ["@lsp.type.punctuation"] = { link = "Delimiter" },
 
         ["@lsp.type.selfParameter"] = { link = "@variable.builtin" },
         -- ["@lsp.type.builtinConstant"] = { link = "@constant.builtin" },
         ["@lsp.type.builtinConstant"] = { link = "@constant.builtin" },
         ["@lsp.type.magicFunction"] = { link = "@function.builtin" },
 
-
         ["@lsp.mod.readonly"] = { link = "Constant" },
         ["@lsp.mod.typeHint"] = { link = "Type" },
         -- ["@lsp.mod.defaultLibrary"] = { link = "Special" },
         -- ["@lsp.mod.builtin"] = { link = "Special" },
 
-
-        ["@lsp.typemod.operator.controlFlow"] = { link = "@keyword.exception" },
+        ["@lsp.typemod.operator.controlFlow"] = { link = "@keyword.exception" }, -- rust ? operator
         ["@lsp.typemod.keyword.documentation"] = { link = "Special" },
 
         ["@lsp.typemod.variable.global"] = { link = "Constant" },
@@ -46,16 +48,9 @@ function M.setup(colors, config)
         ["@lsp.typemod.function.defaultLibrary"] = { link = "@function.builtin" },
         ["@lsp.typemod.method.defaultLibrary"] = { link = "@function.builtin" },
 
-        ["@lsp.typemod.operator.injected"] = { link = "Operator" },
-        ["@lsp.typemod.string.injected"] = { link = "String" },
         ["@lsp.typemod.variable.injected"] = { link = "@variable" },
 
         ["@lsp.typemod.function.readonly"] = { fg = theme.syn.fun, bold = true },
-
-        ["@lsp.typemod.const.injected"] = { link = "Constant" },
-        ["@lsp.typemod.comparison.injected"] = { link = "Operator" },
-        ["@lsp.typemod.bitwise.injected"] = { link = "Operator" },
-        ["@lsp.typemod.punctuation.injected"] = { link = "Delimiter" },
     }
 end
 
